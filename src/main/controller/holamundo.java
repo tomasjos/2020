48060925daf3020cb36f67170a7a3fcb620cdb40
Peter Fight <alberto.piedrafits@gmail.com>
2020-04-13 01:25:06 +0200

    Funcionando en Open JDK 8. Servlet/Controller básico

---
+++ src/main/controller/holamundo.java
@@ -0,0 +1,30 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Servlet2")
+public class holamundo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (PrintWriter writer = response.getWriter()) {
+            writer.println("<!DOCTYPE html><html>");
+            writer.println("<head>");
+            writer.println("<meta charset=\"UTF-8\" />");
+            writer.println("<title>Hola</title>");
+            writer.println("</head>");
+            writer.println("<body>");
+
+            writer.println("<h1>Hola Mundo!</h1>");
+
+            writer.println("</body>");
+            writer.println("</html>");
+        }
+    }
+}
